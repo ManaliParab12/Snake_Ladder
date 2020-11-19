8f87e0e20fd17171b7ec520cc87a85f022da1958
ManaliParab12 <manaliparab10@gmail.com>
2020-11-19 10:15:01 +0530

    Created object of SnakeLadder class to call rollDie method

--- src/snakeLadder/SnakeLadder.java
+++ src/snakeLadder/SnakeLadder.java
@@ -2,19 +2,24 @@ package snakeLadder;
 
 import java.util.Random;
 
+
 public class SnakeLadder {
 	
 	int Position = 0;
-	
-	public static void main(String []args) {
-				
-		System.out.println("Welcome to Snake & Ladder Game");
-	}
 	
 	public int rollDice() {
 		Random r = new Random();
 		int Die = r.nextInt(6) + 1;
 		return Die;
 	}	
+	
+	public static void main(String []args) {
+		
+		System.out.println("Welcome to Snake & Ladder Game");
+		
+		SnakeLadder snakeLadder = new SnakeLadder();		
+		snakeLadder.rollDice();
+	}
+	
 
 }
