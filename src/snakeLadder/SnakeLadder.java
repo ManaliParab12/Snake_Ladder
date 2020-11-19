e3b27b3bb4b7d1bec49c4e31fb1d666c09636119
ManaliParab12 <manaliparab10@gmail.com>
2020-11-19 10:02:01 +0530

    Welcome to Snake & Ladder Game

---
+++ src/snakeLadder/SnakeLadder.java
@@ -0,0 +1,12 @@
+package snakeLadder;
+
+public class SnakeLadder {
+	
+int Position = 0;	
+	
+	public static void main(String []args) {
+				
+		System.out.println("Welcome to Snake & Ladder Game");
+	}
+
+}
