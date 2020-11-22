3fe2a0dad1cc42593223a8715ac4a072554dbbb2
ManaliParab12 <manaliparab10@gmail.com>
2020-11-22 20:33:57 +0530

    Played the game with 2 player

--- src/snakeLadder/SnakeLadder.java
+++ src/snakeLadder/SnakeLadder.java
@@ -7,13 +7,15 @@ public class SnakeLadder {
 	static int Position = 0;
 	static int dieValue;
 	static int noOfRolls = 0;
+	  public static int player1Position=0;
+      public static int player2Position=0;
 	
-	public int rollDie() {
+	public static  int rollDie() {
 		Random r = new Random();
 		int Die = r.nextInt(6) + 1;
 		return Die;
 	}	
-	public int playOption() {
+	public static int playOption() {
 		int newPosition = 0;
 		while(Position != 100) {
 			Position = Position;
@@ -42,28 +44,53 @@ public class SnakeLadder {
 				}
 			noOfRolls++;
 			}
-		System.out.println("Number of times the dice was played to win the game : " + noOfRolls);
 		System.out.println("Player Position" + Position);
 		return newPosition;
 	}
 	
 	public static int ladder() {
-		Position = Position + dieValue;
+		if(Position + dieValue < 100) {
+			Position = Position + dieValue;
+			if(Position < 100) {
+			dieValue = rollDie();
+			}
+			if(Position + dieValue <=100){
+				Position = Position + dieValue;
+				}
+			}
 		return Position;	
 	}
 	
 	public static int snake() {
-		Position = Position - dieValue;
+		if(Position - dieValue < 0) {
+		Position = 0;
+		}
+		else {
+			Position = Position - dieValue;
+		}
 		return Position;	
 	}
-			
+		
 	public static void main(String []args) {
 		
 		System.out.println("Welcome to Snake & Ladder Game");
 		
-		SnakeLadder snakeLadder = new SnakeLadder();		
-		snakeLadder.rollDie();
-		snakeLadder.playOption();
+		SnakeLadder snakeLadder = new SnakeLadder();
 		
+		snakeLadder.rollDie();
+		while (player1Position < 100 && player2Position < 100) {
+			player1Position=playOption();
+			System.out.println("Number of times the dice was played to win the game : " + noOfRolls);
+			System.out.println("Position of Player 1 : " + player1Position); 
+			
+			player2Position=playOption();
+			System.out.println("Number of times the dice was played to win the game : " + noOfRolls);
+			System.out.println("Position of Player 2 : " + player2Position); 			
+		}
+		 if(player1Position == 100) {
+             System.out.println("Player 1 is Winner");
+         }else {
+             System.out.println("Player 2 is Winner");
+         }		
 	}
 }
