bdbf3b8599aafb00ec269199e9a3d33f86bab447
ManaliParab12 <manaliparab10@gmail.com>
2020-11-22 19:54:03 +0530

    Increament noOfDieRoll to get the number of times the dice was played to win the game

--- src/snakeLadder/SnakeLadder.java
+++ src/snakeLadder/SnakeLadder.java
@@ -6,6 +6,7 @@ public class SnakeLadder {
 	
 	static int Position = 0;
 	static int dieValue;
+	static int noOfRolls = 0;
 	
 	public int rollDie() {
 		Random r = new Random();
@@ -16,38 +17,41 @@ public class SnakeLadder {
 		int newPosition = 0;
 		while(Position != 100) {
 			Position = Position;
-		dieValue = rollDie();
-		System.out.println("Die Value" +dieValue);						
-		Random r = new Random();
-		int option = r.nextInt(3);
-		
-		if(option == 0) {			
-			System.out.println("No Play");
-			newPosition = Position;
-		}
-		else if (option == 1) {
+			dieValue = rollDie();
+			System.out.println("Die Value" +dieValue);						
+			Random r = new Random();
+			int option = r.nextInt(3);
 			
-			System.out.println("You are currently on Ladder ");
+			if(option == 0) {			
+				System.out.println("No Play");
+				newPosition = Position;
+				}
+			else if (option == 1) {
+				System.out.println("You are currently on Ladder ");
 				newPosition = ladder();			
+				}
+			else if (option == 2) {
+				System.out.println("You are currently on Snake ");
+				newPosition = snake();
+				}	
+			if(Position < 0) {
+				Position = 0;
+				}
+			if(Position > 100) {
+				Position = Position;
+				}
+			noOfRolls++;
 			}
-		else if (option == 2) {
-			System.out.println("You are currently on Snake ");
-			newPosition = snake();
-		}
-		}	
-		if(Position < 0) {
-			Position = 0;
-		}
-		if(Position > 100) {
-			Position = Position;
-		}
+		System.out.println("Number of times the dice was played to win the game : " + noOfRolls);
 		System.out.println("Player Position" + Position);
 		return newPosition;
 	}
+	
 	public static int ladder() {
 		Position = Position + dieValue;
 		return Position;	
 	}
+	
 	public static int snake() {
 		Position = Position - dieValue;
 		return Position;	
